1c04e1eb0bd0180f95e168b6a2b75d9b45b611c9
Richard Opálka <opalka.richard@gmail.com>
2020-12-15 21:14:15 +0100

    Introducing ServiceListener

---
+++ src/main/java/org/fossnova/mc/ServiceListener.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2012-2019, FOSS Nova Software Foundation (FNSF),
+ * and individual contributors as indicated by the @author tags.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.fossnova.mc;
+
+/**
+ * A listener for service lifecycle events. The associated controller
+ * will not leave its current state until all listeners finish running.
+ *
+ * @author <a href="mailto:ropalka@redhat.com">Richard Opalka</a>
+ */
+public interface ServiceListener {
+    /**
+     * The service has entered new state
+     *
+     * @param controller the controller
+     * @param event the lifecycle event
+     */
+    void handleEvent(ServiceController controller, ServiceEvent event);
+}
