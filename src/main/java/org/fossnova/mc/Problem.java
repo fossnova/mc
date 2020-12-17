b241beb42640aa8ba661987b2c998708e7ecf880
Richard Opálka <opalka.richard@gmail.com>
2020-12-17 20:31:52 +0100

    Extracting inner class Problem.Severity -> Severity

--- src/main/java/org/fossnova/mc/Problem.java
+++ src/main/java/org/fossnova/mc/Problem.java
@@ -91,29 +91,4 @@ public final class Problem implements Serializable {
         return reason;
     }
 
-    /**
-     * Problem severities.
-     */
-    public enum Severity {
-        /**
-         * This problem will not cause undesirable effects but it is something the user should be made aware of
-         * - the service <B>DON'T NEED TO</B> fail in this case.
-         */
-        INFO,
-        /**
-         * This problem could possibly cause undesirable effects now or in the future
-         * - the service <B>SHOULD</B> fail in this case.
-         */
-        WARNING,
-        /**
-         * This problem will likely cause undesirable effects now or in the future
-         * - the service <B>HAVE TO</B> fail in this case.
-         */
-        ERROR,
-        /**
-         * This problem will cause irreparable damage to the system integrity
-         * - the service <B>HAVE TO</B> fail in this case.
-         */
-        CRITICAL
-    }
 }
